8436801f8110626052d4d949edd7755106ed07fd
saul alejos garay <43302778+saulwolfdev@users.noreply.github.com>
2019-09-27 16:38:03 -0300

    Updates

---
+++ precache-manifest.f19b5696c9f015fd51b8e0f6af1db5b0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62cbb5e4c699a40cca2f4a5e30caf4d2",
+    "url": "/Svg-Gsap-React/index.html"
+  },
+  {
+    "revision": "0824ec775246b00b4801",
+    "url": "/Svg-Gsap-React/static/css/main.d5a56aaf.chunk.css"
+  },
+  {
+    "revision": "f19c528a053fbd85803d",
+    "url": "/Svg-Gsap-React/static/js/2.dd0911fa.chunk.js"
+  },
+  {
+    "revision": "0824ec775246b00b4801",
+    "url": "/Svg-Gsap-React/static/js/main.051bc22f.chunk.js"
+  },
+  {
+    "revision": "36b768367848a0fd5295",
+    "url": "/Svg-Gsap-React/static/js/runtime~main.bdacf21d.js"
+  },
+  {
+    "revision": "f2ed261d159b9a75805ce0c71de75013",
+    "url": "/Svg-Gsap-React/static/media/Bandeja.f2ed261d.svg"
+  },
+  {
+    "revision": "3c8e9e1d719e3c66e8a6de89671a1da8",
+    "url": "/Svg-Gsap-React/static/media/Captus1.3c8e9e1d.svg"
+  },
+  {
+    "revision": "2714264ebbc2ac8a0f62c0ce9c7fc5c8",
+    "url": "/Svg-Gsap-React/static/media/Captus2.2714264e.svg"
+  },
+  {
+    "revision": "3498f01fe0addeed7392038f0982a78d",
+    "url": "/Svg-Gsap-React/static/media/Captus3.3498f01f.svg"
+  },
+  {
+    "revision": "352c3e5d199c4f0810f7991a3b9123b2",
+    "url": "/Svg-Gsap-React/static/media/Linea.352c3e5d.svg"
+  },
+  {
+    "revision": "b6b0b74952dc1b56cde95f5d509cddcf",
+    "url": "/Svg-Gsap-React/static/media/Planta.b6b0b749.svg"
+  },
+  {
+    "revision": "ffa89cc69dd2e18e58e06f29ec572958",
+    "url": "/Svg-Gsap-React/static/media/Sol.ffa89cc6.svg"
+  }
+]);
